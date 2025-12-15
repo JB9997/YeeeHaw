46447298e614a7ec0cb04c0153f396df0583b761
JB9997 <jerome.baechtold@stud.bfstg.ch>
2025-12-15 20:09:46 +0100

    Werkzeug
    Aufladen in GitHub

---
+++ Werkzeug/Werkzeug/Models/Werkzeug.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Werkzeug.Models;
+
+public partial class Werkzeug
+{
+    public int WerkzeugId { get; set; }
+
+    public string Bezeichnung { get; set; } = null!;
+
+    public string Kategorie { get; set; } = null!;
+
+    public string Hersteller { get; set; } = null!;
+
+    public int Kaufjahr { get; set; }
+
+    public string Zustand { get; set; } = null!;
+
+    public decimal Preis { get; set; }
+}
